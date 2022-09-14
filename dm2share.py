36b0f2bce5d18148e6b6f379e95a98ba75704e4c
DmHack3ng <72685996+DmHack3ng@users.noreply.github.com>
2022-09-14 21:41:01 +0400

    Update dm2share.py
    Nouvelles MAJ

--- dm2share.py
+++ dm2share.py
@@ -35,11 +35,10 @@ def dm2share_run():
     else:
       v=subprocess.check_output("arp -a",shell=True).decode()# linux(ubuntu)
     
-    #print(v)
+   
     p=re.findall("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}",v)
     for h in p:
       host=h
-      #print(host)
       v=port_scanner(host)
       if v==0:
          html_c=get_content(host)
@@ -71,15 +70,10 @@ def download_files(url_file):
 def beauty_print(ip,data):
    print("\t-----------FILES-------------\n")
    nb=0
-   # >1 : sup a 1
-   # <1 : inf a 1
    if len(data)==1:
-       #print("1 seul fichier"+str(len(data)))
        link_f="http://"+ip+":8000"+"/"+data[0].get('href')
        download_files(link_f)
    else:
-      #print("plusieurs "+str(len(data)))
-      #print(data)
       for a in data:
          name=a.get('href')
          print(str(nb)+"--> "+urllib.parse.unquote(name))
@@ -90,10 +84,8 @@ def beauty_print(ip,data):
 def get_link(ip,dt):
    nbt=interactive_shell()
    link_f="http://"+ip+":8000"+"/"+urllib.parse.unquote(dt[nbt].get('href'))
-   #print(link_f)
    download_files("\n"+link_f)
 
-#dm2share_run()
 
 try:
    check_options()
